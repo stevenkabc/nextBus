f0acd78a192167f6ff4791112f7d801596beea62
Steven Tessler <stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com>
2016-11-08 11:45:00 -0500

    update all

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import * as stations from './stations.js';
+import {routes} from './stations';
 // const buses =
 
-console.log(stations.buses);
+const getMins = (a) => {
+        let ampm, index, minutes, hours, time;
+        ampm = a.includes("AM") ? "AM" : "PM";
+        index = a.indexOf(":");
+        minutes = parseInt(a.slice(index+1, index +3));
+        hours = parseInt(a.slice(0,index));
+
+        if(ampm === "AM" && hours === 12) hours = 24;
+        if(ampm === "PM" && hours < 12) hours = hours + 12;
+        time = hours * 100 + minutes;
+        // console.log(time);
+        // console.log(a );
+        return time;
+}
+
+
+const BusStops = (props) => {
+    var day="";
+    let d=new Date();;
+    let today = d.getDay();
+    // let timeNow = `${d.getHours() -12}:${d.getMinutes()} PM`
+    // timeNow = "8:30 PM"
+    // timeNow = Date(timeNow);
+    // timeNow = Date.parse(timeNow);
+    // var busStopArray = [];
 
 
 
-const BusStops  = (props) => {
+    if (!(today % 5)) {
+      day = 'Saturday';
+    }
+    else if ( !(today % 6)) {
+      day = 'Sunday';
+    }
+    else {
+      day = 'Daily';
+    }
+    // busStopArray = props.busStop[day];
   return (
     <div className="Content">
       {props.busStop.name}
       <div>
-        {props.busStop.time.map((eta,index) => {
-          let route = props.busStop.route[index];
-          return (
-            <div>
-            <div style={{display: 'inline-block'}} >
-            {eta}-{route}</div>
-            </div>
+        { props.busStop[day]['time'].map((eta,index) => {
+          let route = props.busStop[day]['route'][index];
+          let etaDate = new Date(eta);
+          let timeNow = `${d.getHours() }:${d.getMinutes()} PM`
+          //             timeNow ="7:40 PM"
 
-          )
-        },props.busStop)}
+          //var nowMins = d.getHours() * 60 + d.getMinutes();
+          var nowMins = getMins(timeNow);
+          //             var nowMins = 18 * 60 + 30;
+          // var i = eta.indexOf(':');
+          // var hours = eta.slice(0,i);
+          //
+          // if (eta.includes('PM') && (hours < 12) ) {
+          //   hours = 12 * 60 + hours * 60
+          // }
+          // else {
+          //   hours = hours * 60;
+          //   }
+          //   var etaMins = hours + (eta.slice(i+1,i+3) * 1);
+          var etaMins = getMins(eta);
+          var uName = props.busStop.name.toUpperCase()
+
+          if( nowMins < etaMins  && (uName.includes(props.queryStr) ) ) {
+                return(
+                  <div>
+                  <div style={{display: 'inline-block'}} >
+                    {eta}-{route}</div>
+                  </div>
+                )
+              }
+              else {
+                return (
+               <div ></div>
+             )
+           }
+        })}
 
       </div>
 
@@ -37,7 +96,7 @@ const BusRoutes = (props) => {
       {props.routeNumber} - {props.routeName}
     </h1>
       {props.busStops.map( (busStop) =>
-        <BusStops busStop={busStop} />
+        <BusStops busStop={busStop} queryStr={props.queryStr}/>
       )}
     </div>
 
@@ -46,15 +105,17 @@ const BusRoutes = (props) => {
 }
 
 
+
 class WheresMyBus extends Component {
   render() {
     return (
       <div>
-        {this.props.buses.map(function(route) {
+        {this.props.routes.map(function(route) {
           return (
             <BusRoutes routeNumber={route.routeNumber}
             routeName={route.routeName}
-            busStops={route.busStops} />
+            busStops={route.BusStops}
+            queryStr={ ''.toUpperCase()} />
           )
         }
         )}
@@ -72,7 +133,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <WheresMyBus buses={stations.buses} />
+        <WheresMyBus routes={routes} />
 
       </div>
     );
